74c202cf36fdb27e5681836f1ced9c3924650dc5
reedv <villanuevareed@gmail.com>
2016-10-15 14:45:55 -1000

    add contact and edit contact mockups done

--- app/imports/ui/pages/index.js
+++ app/imports/ui/pages/index.js
@@ -1,6 +1,12 @@
 import './add-stuff-page.html';
 import './add-stuff-page.js';
 
+import './add-contact-page.html';
+import './add-contact-page.js';
+
+import './edit-contact-page.html';
+import './edit-contact-page';
+
 import './edit-stuff-page.html';
 import './edit-stuff-page.js';
 
@@ -8,3 +14,5 @@ import './home-page.html';
 
 import './list-stuff-page.html';
 import './list-stuff-page.js';
+
+
